3ff8df9a5060de1ad77966b78cb189bd822b0433
Anton Lindstrom <lindztr@gmail.com>
2017-08-18 21:05:08 +0200

    Bump version since vendoring dependencies

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "envdo"
 	app.Usage = "Manage environment variables efficiently."
-	app.Version = "0.1.0"
+	app.Version = "0.1.1"
 	app.Author = "Anton Lindstrom"
 
 	homeDir, err := homedir.Dir()
